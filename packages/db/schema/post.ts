51944076f28ae0300ea6b9c06f7aa7ea57cc9f08
Sasin91 <jonas.kerwin.hansen@gmail.com>
2023-11-01 20:03:45 +0100

    fixed db migration for mariadb

--- packages/db/schema/post.ts
+++ packages/db/schema/post.ts
@@ -1,12 +1,12 @@
 import { sql } from "drizzle-orm";
-import { serial, timestamp, varchar } from "drizzle-orm/mysql-core";
+import { int, timestamp, varchar } from "drizzle-orm/mysql-core";
 
 import { mySqlTable } from "./_table";
 
 export const post = mySqlTable("post", {
-  id: serial("id").primaryKey(),
-  title: varchar("name", { length: 256 }).notNull(),
-  content: varchar("content", { length: 256 }).notNull(),
+    id: int("id").primaryKey().autoincrement(),
+    title: varchar("name", { length: 255 }).notNull(),
+    content: varchar("content", { length: 255 }).notNull(),
   createdAt: timestamp("created_at")
     .default(sql`CURRENT_TIMESTAMP`)
     .notNull(),
