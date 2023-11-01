4ed401f3a1507cf02cd2741050fb14db9a1c4e3d
Sasin91 <jonas.kerwin.hansen@gmail.com>
2023-11-01 21:26:19 +0100

    pnpm format:fix

--- packages/db/schema/post.ts
+++ packages/db/schema/post.ts
@@ -4,9 +4,9 @@ import { int, timestamp, varchar } from "drizzle-orm/mysql-core";
 import { mySqlTable } from "./_table";
 
 export const post = mySqlTable("post", {
-    id: int("id").primaryKey().autoincrement(),
-    title: varchar("name", { length: 255 }).notNull(),
-    content: varchar("content", { length: 255 }).notNull(),
+  id: int("id").primaryKey().autoincrement(),
+  title: varchar("name", { length: 255 }).notNull(),
+  content: varchar("content", { length: 255 }).notNull(),
   createdAt: timestamp("created_at")
     .default(sql`CURRENT_TIMESTAMP`)
     .notNull(),
