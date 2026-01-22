1d6e84e054993a4b3b3a58ea40251dad9a1840e7
sasin91 <jonas.kerwin.hansen@gmail.com>
2026-01-22 17:09:54 +0100

    add lift label

--- app/Training/Lift.php
+++ app/Training/Lift.php
@@ -4,9 +4,15 @@ namespace App\Training;
 
 class Lift
 {
+    public string $label;
+
     public function __construct(
         public int $sets,
         public int $reps,
-        public float $weight
-    ) {}
+        public float $weight,
+        ?string $label = null,
+    ) 
+    {
+        $this->label = $label ?? sprintf('%d x %d @ %.1fkg', $sets, $reps, $weight);
+    }
 }
