55580bce9680e953311fc24b092fd52ad2cf8779
sasin91 <jonas.kerwin.hansen@gmail.com>
2026-01-25 20:23:48 +0100

    added make:training:program and make:training:exercise commands

---
+++ app/Console/Commands/MakeExercise.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\GeneratorCommand;
+use Illuminate\Support\Str;
+
+class MakeExercise extends GeneratorCommand
+{
+    protected $signature = 'make:training:exercise {name}';
+
+    protected $description = 'Create a new training exercise class';
+
+    protected $type = 'Exercise';
+
+    protected function getStub(): string
+    {
+        return resource_path('stubs/training.exercise.stub');
+    }
+
+    protected function getDefaultNamespace($rootNamespace): string
+    {
+        return $rootNamespace.'\Training\Exercises';
+    }
+
+    protected function buildClass($name): string
+    {
+        $stub = parent::buildClass($name);
+
+        $label = Str::of(class_basename($name))->headline();
+        $slug = Str::slug($label);
+
+        return str_replace(
+            ['{{ slug }}', '{{ label }}'],
+            [$slug, $label],
+            $stub
+        );
+    }
+}
